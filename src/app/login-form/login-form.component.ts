75831702038d81fa55b8bcab1d620ee82da11b3b
hummingbird-12 <entropyih@sogang.ac.kr>
2020-12-01 07:34:16 +0900

    Implement onboarding components and services

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { AuthService } from '../auth.service';
+import { ModalService } from '../modal.service';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.css']
+  styleUrls: ['./login-form.component.css'],
 })
 export class LoginFormComponent implements OnInit {
+  loginForm: FormGroup = new FormGroup({
+    username: new FormControl('', {
+      validators: [Validators.required, Validators.email],
+      updateOn: 'submit',
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required],
+      updateOn: 'submit',
+    }),
+  });
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private modalService: ModalService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onLogin(): void {
+    if (!this.loginForm.valid) {
+      return;
+    }
+    const username = this.loginForm.get('username')?.value;
+    const password = this.loginForm.get('password')?.value;
+    this.authService.postLogin(username, password).subscribe((res) => {
+      if (res) {
+        this.router.navigate(['/app']);
+      } else {
+        this.modalService.showModal('Login failed', [
+          {
+            text: 'Close',
+            context: 'secondary',
+            handler: () => {
+              this.modalService.closeModal();
+            },
+          },
+        ]);
+      }
+    });
   }
-
 }
--- src/app/register-form/register-form.component.ts
+++ src/app/register-form/register-form.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { AuthService } from '../auth.service';
+import { ModalService } from '../modal.service';
 
 @Component({
   selector: 'app-register-form',
@@ -6,7 +11,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-form.component.css'],
 })
 export class RegisterFormComponent implements OnInit {
-  constructor() {}
+  registerForm: FormGroup = new FormGroup({
+    name: new FormControl('', {
+      validators: [Validators.required],
+      updateOn: 'submit',
+    }),
+    username: new FormControl('', {
+      validators: [Validators.required, Validators.email],
+      updateOn: 'submit',
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required],
+      updateOn: 'submit',
+    }),
+    passwordVerif: new FormControl('', {
+      validators: [Validators.required],
+      updateOn: 'submit',
+    }),
+  });
+
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private modalService: ModalService
+  ) {}
 
   ngOnInit(): void {}
+
+  onJoin(): void {
+    if (!this.registerForm.valid) {
+      return;
+    }
+
+    const name = this.registerForm.get('name')?.value;
+    const username = this.registerForm.get('username')?.value;
+    const password = this.registerForm.get('password')?.value;
+    const passwordVerif = this.registerForm.get('passwordVerif')?.value;
+    if (password !== passwordVerif) {
+      return;
+    }
+    this.authService.postSignup(username, password).subscribe((res) => {
+      if (res) {
+        this.router.navigate(['/app']);
+      } else {
+        this.modalService.showModal('Registeration failed', [
+          {
+            text: 'Close',
+            context: 'secondary',
+            handler: () => {
+              this.modalService.closeModal();
+            },
+          },
+        ]);
+      }
+    });
+  }
 }
